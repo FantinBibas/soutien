c7593e1a54d7d89e2883de9aafbb005dd6fc2938
Quentin Sanchez <quentin.sanchez@epitech.eu>
2017-03-23 15:38:28 +0100

    Plop

---
+++ 2017.03.23/print-as-struct/main.c
@@ -0,0 +1,23 @@
+/*
+** main.c for  in /home/quentin.sanchez/Delivery/Clone/Oui
+**
+** Made by Quentin Sanchez
+** Login   <quentin.sanchez@epitech.net>
+**
+** Started on  Thu Mar 23 13:40:15 2017 Quentin Sanchez
+** Last update Thu Mar 23 13:48:00 2017 Quentin Sanchez
+*/
+
+typedef struct s_léphone
+{
+}		t_léphone;
+
+void	print_as_struct(const int fd, t_léphone buff)
+{
+
+}
+
+int	main()
+{
+
+}
